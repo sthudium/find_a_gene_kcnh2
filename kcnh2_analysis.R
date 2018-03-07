b319556a4172c4bc8be8d0187b0210292d2c997d
Sam Thudium <sthudium@ucsd.edu>
2018-03-06 23:33:23 -0800

    Read in the fasta version of frist MUSCLE aln

---
+++ kcnh2_analysis.R
@@ -0,0 +1,13 @@
+library(bio3d)
+
+?muscle
+??hmmer
+
+?seqaln
+
+aln.test <- read.fasta("~/Desktop/test_muscle_aln.fst")
+head(aln.test)
+
+aln.kcnh2 <- read.fasta("muscle_aln_kcnh2.fst")
+View(aln.kcnh2)
+
