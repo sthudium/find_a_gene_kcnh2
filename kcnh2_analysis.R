13cc0a469f889af64c845a4be41ff0f49107b605
Sam Thudium <sthudium@ucsd.edu>
2018-03-08 09:08:49 -0800

    look up structures related to human kcnh2

--- kcnh2_analysis.R
+++ kcnh2_analysis.R
@@ -18,4 +18,8 @@ aln.kcnh2.consen
 
 kcnh2.blast <- blast.pdb("JK291807.1")
 
-kcnh2.hmmr <- hmmer("muscle_aln_kcnh2_trim.fst")
+kcnh2.hmmr <- hmmer(aln.kcnh2, db = "nr")
+kcnh2.hmmr
+
+kcnh2.blastx2 <- blast.pdb("AAI27674.1")
+kcnh2.blastx2$hit.tbl$subjectids
