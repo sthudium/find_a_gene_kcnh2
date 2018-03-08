fde82ca5d41c728e749fbaa4d91561677e4bd286
Sam Thudium <sthudium@ucsd.edu>
2018-03-07 19:19:39 -0800

    trying hmmer search on the fasta sequences

--- kcnh2_analysis.R
+++ kcnh2_analysis.R
@@ -8,5 +8,14 @@ library(bio3d)
 aln.test <- read.fasta("~/Desktop/test_muscle_aln.fst")
 head(aln.test)
 
+## pdb search on novel protein instead of the human sequence
 
+View(aln.kcnh2)
 
+aln.kcnh2.cutoff <- aln.kcnh2$ali[ , unlist(consensus(aln.kcnh2))]
+aln.kcnh2.consen <- consensus(aln.kcnh2)
+aln.kcnh2.consen
+
+kcnh2.blast <- blast.pdb("JK291807.1")
+
+kcnh2.hmmr <- hmmer("muscle_aln_kcnh2_trim.fst")
